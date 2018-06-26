665f42074ff2f3419db60ea1b327e6b7c152a69f
DS <enthalpy3@gmail.com>
2018-06-26 10:07:56 +0900

    first commit

---
+++ addr/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Addr(models.Model):
+    name = models.CharField(max_length=30, blank=True)
+    phone1 = models.CharField(max_length=15)
+    phone2 = models.CharField(max_length=15, blank=True)
+    email = models.EmailField(max_length=70, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    # published_date = models.DateTimeField(blank=True, null=True)
+    # title = models.CharField(max_length=200)
+    # text = models.TextField()
+    # on_off = models.NullBooleanField(default=True)
+
+    def create(self):
+        self.created_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.name
