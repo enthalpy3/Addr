a69e96f8085a089e3b941573ee07e9d1e5669f01
DS <enthalpy3@gmail.com>
2018-06-26 15:53:22 +0900

    중 완료 승인된 주소만 보여주기 7차수정

--- addr/models.py
+++ addr/models.py
@@ -9,19 +9,19 @@ class Addr(models.Model):
     email = models.EmailField(max_length=70, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    approved_addr = models.BooleanField(default=False)
-    # published_date = models.DateTimeField(blank=True, null=True)
+    # approved_addr = models.BooleanField(default=False)
+    published_date = models.DateTimeField(blank=True, null=True)
     # title = models.CharField(max_length=200)
     # text = models.TextField()
     # on_off = models.NullBooleanField(default=True)
 
-    # def publish(self):
-    #     self.created_date = timezone.now()
-    #     self.save()
-
-    def approve(self):
-        # self.approved_addr = True
+    def publish(self):
+        self.published_date = timezone.now()
         self.save()
+
+    # def approve(self):
+    #     self.approved_addr = True
+    #     self.save()
 
     def __str__(self):
         return self.name
